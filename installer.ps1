100758bbbcbdba9206ec51135a7654fa7b872b52
realkolobok <kolobokkitajskij@gmail.com>
2025-11-09 16:23:40 +0200

    Add powershell script to automate updates

---
+++ installer.ps1
@@ -0,0 +1,25 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+$remoteUrl = "https://github.com/realkolobok/Excel_AddIn/raw/refs/heads/main/CustomAddIn_1.xlam"
+$dest = "$env:APPDATA\Microsoft\AddIns\CustomAddIn_1.xlam"
+
+if (Test-Path $dest) {
+    $overwrite = [System.Windows.Forms.MessageBox]::Show(
+        "The add-in file already exists at:`n$dest`nDo you want to overwrite it?",
+        "Confirm Overwrite",
+        [System.Windows.Forms.MessageBoxButtons]::YesNo,
+        [System.Windows.Forms.MessageBoxIcon]::Question
+    )
+    if ($overwrite -ne [System.Windows.Forms.DialogResult]::Yes) {
+        Write-Host "Operation cancelled by user."
+        exit
+    }
+}
+
+Invoke-WebRequest $remoteUrl -OutFile $dest -UseBasicParsing
+[System.Windows.Forms.MessageBox]::Show(
+    "The add-in was installed successfully!",
+    "Excel Add-In Installer",
+    [System.Windows.Forms.MessageBoxButtons]::OK,
+    [System.Windows.Forms.MessageBoxIcon]::Information
+)
